b4fecd2eab08dee60a8c1d0cf1c55295162d4851
Nicolas Bugeja <nicolas.bugeja@palpix.com>
2020-01-10 09:39:38 +0100

    Premier essai connexion bluetooth => Matrice

--- cornhole/cornhole.ino
+++ cornhole/cornhole.ino
@@ -1,12 +1,20 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
+#include <SoftwareSerial.h>
+
+//Serial port
+#define HEADER        '|'
+#define MESSAGE_BYTES  2  // the total bytes in a message
+char state = 'S';
 
 #ifndef PSTR
  #define PSTR // Make Arduino Due happy
 #endif
 
 #define PIN 2
+
+SoftwareSerial hc06(4, 5);
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
   NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
@@ -20,10 +28,9 @@ int scoreBlue = 0;
 int scoreRed = 0;
 
 void showScore(String blue, String red) {
-  matrix.begin();
   matrix.fillScreen(0);
   matrix.setTextWrap(false);
-  matrix.setBrightness(20);
+  matrix.setBrightness(10);
   matrix.setTextSize(1);
   
   matrix.setTextColor(colors[3]);
@@ -41,22 +48,55 @@ void showScore(String blue, String red) {
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  
-
+  hc06.begin(9600);
+  matrix.begin();
   showScore((String)scoreBlue, (String)scoreRed);
+
+  Serial.begin(9600);
+  Serial.println("STARTING CORN HOLE 2 TURBO");  
+
+  delay(500);
 }
 
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //delay(1000);
+  char old_state = state;
+  
+  if (hc06.available() >= MESSAGE_BYTES) { // If data is available to read    
+    Serial.println("Available");
+    delay(3);
+    if (hc06.read() == HEADER)
+    {
+      state = (char)hc06.read(); // read it and store it in val
+    }
+  }
 
-  //scoreBlue++;
-  //showScore((String)scoreBlue, (String)scoreRed);
-
-  //delay(1000);
-  //scoreRed++;
-  //showScore((String)scoreBlue, (String)scoreRed);
+  if(old_state != state){
+    Serial.print("Received : ");
+    Serial.println(state);
+    switch (state) {
+      case 'A':
+        scoreBlue = 10;
+        Serial.println("Blue team 10 points");
+        break;
+      case 'B':
+        scoreBlue = 20;
+        Serial.println("Blue team 20 points");
+        break;
+      case 'X':
+        scoreRed = 10;
+        Serial.println("Red team 10 points");
+        break;
+      case 'Y':
+        scoreRed = 20;
+        Serial.println("Red team 20 points");
+        break;
+      default://S
+        Serial.println("resting");
+        break;
+    }
+  
+    showScore((String)scoreBlue, (String)scoreRed);
+  }
 }
